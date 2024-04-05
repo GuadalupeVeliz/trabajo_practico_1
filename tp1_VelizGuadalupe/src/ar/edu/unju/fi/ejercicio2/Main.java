c54831c3d5a22316a23e999674a91b3aae69a412
GuadalupeVeliz <guadalupeveliz100@gmail.com>
2024-04-05 19:51:04 -0300

    Se agrega paquete y clase para el ejercicio 2

---
+++ tp1_VelizGuadalupe/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.ejercicio2;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		String nombre="Argentina";
+		int edad =22 ;
+		float altura= 1.67f;
+		double precio=23.50;
+		String telefono="3888112223";
+		double resultado=Math.cos(0.5);
+		System.out.println("Nombre de un pais: "+ nombre);
+		System.out.println("Edad: "+edad);
+		System.out.println("Altura: "+altura);
+		System.out.println("Precio: "+precio);
+		System.out.println("Telefono: "+telefono);
+		System.out.println("Resultado:"+resultado);
+	}
+}
