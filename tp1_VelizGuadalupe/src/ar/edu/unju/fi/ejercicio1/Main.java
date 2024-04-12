f3696f9202630ca32cf5a54821208dea8e5bdc46
GuadalupeVeliz <guadalupeveliz100@gmail.com>
2024-04-12 15:19:48 -0300

    Creacion del paquete y clase para el ejercicio11

--- tp1_VelizGuadalupe/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ tp1_VelizGuadalupe/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -1,26 +1,12 @@
 package ar.edu.unju.fi.ejercicio1;
 
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		int num1, num2, num3,num4,num5;
-		double promedio;
-		System.out.println("Ingrese un valor para el primer numero: ");
-		num1=sc.nextInt();
-		System.out.println("Ingrese un valor para el segundo numero: ");
-		num2=sc.nextInt();
-		System.out.println("Ingrese un valor para el tercer numero: ");
-		num3=sc.nextInt();
-		System.out.println("Ingrese un valor para el cuarto numero: ");
-		num4=sc.nextInt();
-		System.out.println("Ingrese un valor para el quinto numero: ");
-		num5=sc.nextInt();
+		int num1=8, num2=5, num3=1,num4=20,num5=9;
+		double promedio;	
 		promedio= (num1+num2+num3+num4+num5)/5.0;
 		System.out.println("El promedio es: "+promedio);
-		sc.close();
 	}
 
 }
