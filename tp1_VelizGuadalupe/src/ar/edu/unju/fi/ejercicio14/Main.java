deb655cdcac8ab5b67f249fc8ff79e482b51fcbf
GuadalupeVeliz <guadalupeveliz100@gmail.com>
2024-04-13 12:48:56 -0300

    Creacion del paquete y clase para el ejercicio 15,correccion del ejercicio 14

--- tp1_VelizGuadalupe/src/ar/edu/unju/fi/ejercicio14/Main.java
+++ tp1_VelizGuadalupe/src/ar/edu/unju/fi/ejercicio14/Main.java
@@ -5,26 +5,27 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int cantidad=-1,i,num,suma=0;
-		
-		while(cantidad<3 || cantidad >10) {
-			System.out.println("Ingrese un numero que este entre el rango [3-10]: ");
-			cantidad=sc.nextInt();
+		try (Scanner sc = new Scanner(System.in)) {
+			int cantidad=-1,i,num,suma=0;
+			
+			while(cantidad<3 || cantidad >10) {
+				System.out.println("Ingrese un numero que este entre el rango [3-10]: ");
+				cantidad=sc.nextInt();
+			}
+			int [] numeros= new int[cantidad];
+			for(i=0;i<cantidad;i++) {
+				System.out.println("Ingrese un numero: ");
+				num=sc.nextInt();
+				numeros[i]=num;
+			}
+			sc.close();
+			for(i=0;i<cantidad;i++) {
+				System.out.println("Numero: "+numeros[i]);
+				suma=suma+numeros[i];
+			}
+			System.out.println("Suma: "+suma);
 		}
 		
-		int [] numeros= new int[cantidad];
-		for(i=0;i<cantidad;i++) {
-			System.out.println("Ingrese un numero: ");
-			num=sc.nextInt();
-			numeros[i]=num;
-		}
-		for(i=0;i<cantidad;i++) {
-			System.out.println("Numero: "+numeros[i]);
-			suma=suma+numeros[i];
-		}
-		System.out.println("Suma: "+suma);
-		sc.close();
 	}
 
 }
---
+++ tp1_VelizGuadalupe/src/ar/edu/unju/fi/ejercicio15/Main.java
@@ -0,0 +1,33 @@
+package ar.edu.unju.fi.ejercicio15;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			int num=1,i;
+			String nombre;
+			
+			while(num<5 || num>10) {
+				System.out.println("Ingrese un numero en el rango [5-10]: ");
+				num=sc.nextInt();
+			}
+			
+			String[] nombres= new String[num];
+			for(i=0;i<num;i++) {
+				System.out.println("Ingrese un nombre: ");
+				nombre=sc.next();
+				nombres[i]=nombre;
+			}
+			sc.close();
+			for(i=0;i<num;i++) {
+				System.out.println("Nombre: "+nombres[i]);
+			}
+			System.out.println("********************");
+			for(i=num;i>0;i--) {
+				System.out.println("Nombre: "+nombres[i-1]);
+			}
+		}
+		
+	}
+}
